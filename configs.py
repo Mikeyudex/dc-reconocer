ac5336c400e469ce40f655ba57959745aa29b082
miguelyudex92 <miguel.yudex@gmail.com>
2024-01-23 11:35:13 -0500

    se suben certificados

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ ENV_SERVICE='PROD'
 APP='API DATACREDITO - GALILEA - RECONOCER'
 CONFIGWS = {
     "USUARIO_OKTA_DEV": "2-901582748",
-    "USUARIO_OKTA_PROD": "2-901582748.4@datacredito.com.co",
+    "USUARIO_OKTA_PROD": "2-901582748.4",
     "CLAVE_OKTA_DEV": "Galilea2023",
     "CLAVE_OKTA_PROD": "OktaGalilea#2024",
     "URL_SERVICE_ACIERTA_DEV": "https://demo-servicesesb.datacredito.com.co/wss/localizacion/services/ServicioLocalizacion?wsdl",
